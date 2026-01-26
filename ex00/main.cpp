49aac9683d85a7fac75811608bbdfc6ddbc60a7f
joeow06 <joeow06@gmail.com>
2026-01-26 23:36:34 +0800

    feat: added main file

---
+++ ex00/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jow <jow@student.42kl.edu.my>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/26 22:57:40 by jow               #+#    #+#             */
+/*   Updated: 2026/01/26 22:57:41 by jow              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+int main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		std::cout << "Error: Invalid arguments" << std::endl;
+		return (0);
+	}
+	ScalarConverter::convert(av[1]);
+}
