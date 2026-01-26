6cd6687342c0e1c62e250a89457e1caaa58430fc
joeow06 <joeow06@gmail.com>
2026-01-26 23:36:17 +0800

    feat: added ScalarConverter.cpp

---
+++ ex00/ScalarConverter.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jow <jow@student.42kl.edu.my>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/26 22:57:43 by jow               #+#    #+#             */
+/*   Updated: 2026/01/26 22:57:44 by jow              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter() {}
+
+ScalarConverter::~ScalarConverter() {}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &other)
+{
+	(void) other;
+}
+
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter &other)
+{
+	(void) other;
+	return (*this);
+}
