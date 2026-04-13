cf69f2278bb7d143c93423baf6346d197e0e22ca
joeow06 <joeow06@gmail.com>
2026-04-13 15:25:18 +0800

    feat: added necessary files for ex01

---
+++ ex01/Serializer.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jow <jow@student.42kl.edu.my>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/13 14:45:01 by jow               #+#    #+#             */
+/*   Updated: 2026/04/13 14:45:02 by jow              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERIALIZER_HPP
+#define SERIALIZER_HPP
+
+class Serializer {
+	private:
+		Serializer();
+		~Serializer();
+		Serializer(const Serializer &other);
+		Serializer& operator=(const Serializer &other);
+	public:
+		static uintptr_t serialize(Data* ptr);
+		static Data* deserialize(uintptr_t raw);
+}
+
+#endif
