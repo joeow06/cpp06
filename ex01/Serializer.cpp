d6289843bf828856782f949fa88596c895ffe356
joeow06 <joeow06@gmail.com>
2026-04-13 15:30:58 +0800

    feat: added ocf

--- ex01/Serializer.cpp
+++ ex01/Serializer.cpp
@@ -11,3 +11,18 @@
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
+
+Serializer::Serializer() {}
+
+Serializer::~Serializer() {}
+
+Serializer::Serializer(const Serializer &other)
+{
+	(void) other;
+}
+
+Serializer& Serializer::operator=(const Serializer &other)
+{
+	(void) other;
+	return (*this);
+}
